57161cf91a7258da3c97641c8557a0f1f270fa23
ext_iestrada <ext_iestrada@tottus.com.pe>
2024-04-30 10:26:05 -0500

    Add vote band

--- sockets/socket.js
+++ sockets/socket.js
@@ -1,7 +1,20 @@
 const { io } = require('../index');
+const Band = require('../models/band');
+const Bands = require('../models/bands');
+
+const bands = new Bands();
+// console.log('Init Server...')
+bands.addBand(new Band('Queen'));
+bands.addBand(new Band('Bon Jovi'));
+bands.addBand(new Band('Heroes del Silencio'));
+bands.addBand(new Band('Metallica'));
+
+// console.log(bands);
 
 io.on("connection", (client) => {
   console.log("Cliente conectado");
+
+  client.emit('active-bands', bands.getBands());
 
   client.on("disconnect", () => {
     console.log("Cliente desconectado");
@@ -16,5 +29,11 @@ io.on("connection", (client) => {
     console.log("Mensaje", payload);
     io.emit("nuevo-mensaje", payload); // Emite a todos
     client.broadcast.emit("nuevo-mensaje", payload); // Emite a todos menos al que lo emitio
+  });
+
+  client.on("vote-band", (payload) => {
+    console.log("Mensaje", payload);  
+    bands.voteBand(payload.id);
+    io.emit('active-bands', bands.getBands());
   });
 });
